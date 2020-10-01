e98b21b33db067da62cfdb95c6ffe1de9f84b8cb
Ann Balina <ann.balina@gmail.com>
2020-10-01 19:22:12 +0300

    Init rspec and rswag

--- Gemfile
+++ Gemfile
@@ -28,6 +28,10 @@ gem 'bootsnap', '>= 1.4.2', require: false
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rspec-rails', '~> 4.0.1'
+  gem 'rswag-specs'
+  gem 'rswag-api'
+  gem 'rswag-ui'
 end
 
 group :development do
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/github', to: 'github#authorize'
   get '/auth/github/callback', to: 'github#callback'
@@ -7,4 +8,9 @@ Rails.application.routes.draw do
   get '/repos/:owner/:repo', to: 'repositories#show'
   get '/repos/:owner/:repo/issues', to: 'issues#index'
   get '/repos/:owner/:repo/issues/:number', to: 'issues#show'
+
+  unless Rails.env == 'production'
+    mount Rswag::Ui::Engine => '/api-docs'
+    mount Rswag::Api::Engine => '/api-docs'
+  end
 end
